17dd5febda78472d6d516b81cdf6b786d7963218
Evan Purkhiser <evanpurkhiser@gmail.com>
2014-03-21 22:54:17 -0400

    Attach to the flat-share-container DOMNodeInserted

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,14 +1,41 @@
-var TRACK_SEARCH_URL = 'http://us.7digital.com/search/track?q='
+var SEVEN_DIGITAL_URLS = {
+	'track':   'http://us.7digital.com/search/track?q=',
+	'artist':  'http://us.7digital.com/search/artist?q=',
+	'release': 'http://us.7digital.com/search/release?q=',
+	'other':   'http://us.7digital.com/search?q=',
+}
 
-// The title of the track is in a meta element
-var track = $('meta[name=title]').attr('content'),
-    link  = TRACK_SEARCH_URL + encodeURIComponent(track)
+var setupBeatportButton = function()
+{
+	var context = $('#flat-share-container');
 
-// Setup the link button
-var linkElement = $('<a>')
-	.attr('href', link)
-	.attr('target', '_blank')
-	.addClass('ex7digital-small-button')
-	.text(track)
+	// Get information on what we're searching
+	var title = context.data('title'),
+		query = encodeURIComponent(title),
+	    type  = context.data('context').toLowerCase(),
+	    link  = SEVEN_DIGITAL_URLS[type] + query;
 
-$('#flat-share-container:parent').before(linkElement)
+	// Handle unkown context types (Labels.. etc)
+	if ( ! (link in SEVEN_DIGITAL_URLS))
+	{
+		link = SEVEN_DIGITAL_URLS['other'] + query;
+	}
+
+	// Setup the link button
+	var linkElement = $('<a>')
+		.attr('href', link)
+		.attr('target', '_blank')
+		.attr('title', 'Find this on 7digital')
+		.addClass('ex7digital-small-button')
+		.text(title)
+
+	context.before(linkElement)
+};
+
+document.addEventListener('DOMNodeInserted', function(e)
+{
+	if(e.relatedNode.id !== 'flat-share-container') return;
+
+	setupBeatportButton();
+},
+false);
